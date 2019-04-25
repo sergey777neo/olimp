ba6abfe8949d76fc403ce8d3912e9485ba034f32
ddrakonn <mbrazhenenko@gmail.com>
2019-04-26 01:57:08 +0300

    ?

--- tfs/notify.ps1
+++ tfs/notify.ps1
@@ -6,8 +6,10 @@ $JSON = @'
 { "state": "pending", "target_url": "http://168.62.57.106:8080/tfs/Projects/Olimp2019/_build?_a=summary&buildId=$(Build.BuildNumber)", "description": "The build is running", "context": "continuous-integration/tfs" }
 '@
 
+$URI = "https://api.github.com/repos/ddrakonn/olimp/statuses/$(Build.SourceVersion)"
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri  https://api.github.com/repos/ddrakonn/olimp/statuses/$(Build.SourceVersion) -Method POST -Headers $headers -Body $JSON -MaximumRedirection 0 -ErrorVariable Err
+Invoke-WebRequest -Uri $URI -Method POST -Headers $headers -Body $JSON -MaximumRedirection 0 -ErrorVariable Err
 Write-Host $err[0].InnerException.Response.Headers.Location
 
 
