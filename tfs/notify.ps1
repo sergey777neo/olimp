76c9b4e3bc04f0fddd3072f4ba99af4b9c18e7f3
ddrakonn <mbrazhenenko@gmail.com>
2019-04-26 14:39:37 +0300

    fix

--- tfs/notify.ps1
+++ tfs/notify.ps1
@@ -2,6 +2,7 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add('Content-type','Application/Json')
 $headers.Add('Authorization','token 17f387bc53e2566e68016697c4a24faca2f7b05b')
 
+
 $JSON = @'
 { "state": "pending", "target_url": "http://168.62.57.106:8080/tfs/Projects/Olimp2019/_build?_a=summary&buildId=$(Build.BuildNumber)", "description": "The build is running", "context": "continuous-integration/tfs" }
 '@
@@ -11,7 +12,7 @@ $Source = $Env:BUILD_SOURCEVERSION
 $URI = "https://api.github.com/repos/ddrakonn/olimp/statuses/$Source"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri $URI -Method POST -Headers $headers -Body $JSON -MaximumRedirection 0 -ErrorVariable Err
+Invoke-RestMethod -Uri $URI -Method POST -Headers $headers -Body $JSON -MaximumRedirection 0 -ErrorVariable Err
 Write-Host $err[0].InnerException.Response.Headers.Location
 
 
