36a3525d0d4d9959368cf888cc41dba5fbdf3dd0
Vladislav Loza <vladok001@gmail.com>
2019-04-29 17:28:06 +0300

    Feature/tests (#5)
    * User Story 7: REST API аутентифікації через соціальні мережі
    
    * User Story 12:Головна сторінка
    
    * User Story 12:Головна сторінка

---
+++ Project/Olimp2019.Tests/DefaultTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Olimp2019.Tests
+{
+	[TestClass]
+	public class DefaultTests
+	{
+		[TestMethod]
+		public void StrangeTest() => Assert.AreEqual(1, 1);
+	}
+}
