ebb2115a5c4bcd673b7986a70e9a8b24f37dd96d
ddrakonn <mbrazhenenko@gmail.com>
2019-05-03 00:09:23 +0300

    User Story 70:Налаштування розгортання додатку в Azure (#7)
    * framework switch + factory
    
    * add uploader def + dirs creator
    
    * switch to msbuild
    
    * fix paths

---
+++ Project/Olimp2019.Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Olimp2019.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Olimp2019.Data
+{
+    public class ApplicationDbContextFactory: IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {            
+            
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Olimp2019;Trusted_Connection=True;");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
