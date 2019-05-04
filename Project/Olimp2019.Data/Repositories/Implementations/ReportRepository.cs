6f5cc81965f109dccefff9225d04f93eafbaf451
Vladislav Loza <vladok001@gmail.com>
2019-05-05 01:56:13 +0300

    User Story 45:Головний екран гри (#11)
    * User Story 7: REST API аутентифікації через соціальні мережі
    
    * User Story 12:Головна сторінка
    
    * User Story 12:Головна сторінка
    
    * add test project
    
    * fix
    
    * User Story 9:Сторінка реєстрації, User Story 10:Сторінка входу в систему
    
    * User Story 9:Сторінка реєстрації, User Story 10:Сторінка входу в систему
    
    * ui fixes
    
    * other pages stylization
    
    * reports page
    
    * report users implemented
    
    * require admin role for reports, unit tests
    
    * User Story 45:Головний екран гри

---
+++ Project/Olimp2019.Data/Repositories/Implementations/ReportRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Olimp2019.Data.ViewModels;
+
+namespace Olimp2019.Data.Repositories.Implementations
+{
+    public class ReportRepository : IReportRepository
+    {
+        private List<ReportItemViewModel> _Reports;
+        public List<ReportItemViewModel> Reports
+        {
+            get
+            {
+                if (_Reports == null)
+                {
+                    _Reports = new List<ReportItemViewModel>()
+                    {
+                        new ReportItemViewModel()
+                        {
+                            Name = "ReportUsers",
+                            Caption = Resources.Resources.ReportUser
+                        }
+                    };
+                }
+                return _Reports;
+            }
+        }
+
+        public ReportItemViewModel GetReport(string name)
+        {
+            return this.Reports.SingleOrDefault(r => r.Name == name);
+        }
+    }
+}
